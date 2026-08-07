d140c0e822b4a630c7c2e28b1f91d5048c6b09b1
trool maroc <troolmaroc1@gmail.com>
2026-08-07 20:12:03 +0100

    feat(project): implement project showcase UI

--- src/component/projectDemo.tsx
+++ src/component/projectDemo.tsx
@@ -5,9 +5,9 @@ import logo from "../assets/logo.jpg";
 
 export function ProjectDemo() {
   return (
-    <div className="relative top-30 left-20 w-270">
-      <div className="flex justify-between items-center w-fit">
-        <h1 className="text-white text-3xl">Projects</h1>
+    <section className="relative my-5 top-30 left-10 w-270">
+      <div className="flex py-10 justify-between items-center w-fit">
+        <h1 className="text-white text-3xl font-semibold">Projects</h1>
         <div className="w-160 ml-5 mt-2 border-t-2 border-[#C778DD]" />
         <Link className="inline-flex ml-40 gap-2 text-white" to={"projects"}>
           View all
@@ -15,7 +15,7 @@ export function ProjectDemo() {
         </Link>
       </div>
 
-      <div className="bg-[#282C33] flex gap-10 flex-wrap w-fit mt-4">
+      <div className="bg-[#282C33] pl-2 flex gap-10 flex-wrap w-fit mt-4">
         <ProjectPlaceholder
           img={logo}
           title="ChertNodes"
@@ -38,9 +38,8 @@ export function ProjectDemo() {
           description="Minecraft servers hosting"
           skills={["HTML", "SCSS", "Python", "Flask"]}
           githubUrl="https://github.com/OussamaEl-Asri"
-          liveUrl="https://github.com/OussamaEl-Asri"
         />
       </div>
-    </div>
+    </section>
   );
 }
--- src/component/projectsPlaceholder.tsx
+++ src/component/projectsPlaceholder.tsx
@@ -34,7 +34,9 @@ export function ProjectPlaceholder(projectProps: Project) {
         <div className="flex items-center gap-3 mt-2">
           {projectProps.liveUrl && (
             <Link
-              className="border border-[#C778DD] text-white px-10 py-1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center justify-center border border-[#C778DD] px-10 py-2 text-white rounded-lg transition-all duration-300 hover:bg-[#C778DD] hover:text-[#09090B]"
               to={projectProps.liveUrl}
             >
               Live
@@ -42,7 +44,9 @@ export function ProjectPlaceholder(projectProps: Project) {
           )}
 
           <Link
-            className="border border-[#ABB2BF] text-[#ABB2BF] px-3 py-1"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center justify-center border border-[#ABB2BF] px-3 py-2 text-[#ABB2BF] rounded-lg transition-all duration-300 hover:bg-[#ABB2BF] hover:text-[#282C33] active:scale-95"
             to={projectProps.githubUrl}
           >
             Source Code
